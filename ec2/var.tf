8826d1dfc7787afa4859a281102f09e8f0a0a214
Priyadarshini Itraj <50285249+itrajp@users.noreply.github.com>
2020-08-15 11:20:40 +0530

    Add files via upload

---
+++ ec2/var.tf
@@ -0,0 +1,28 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-13be557e"
+  }
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "INST"{
+    default = "t2.micro"
+}
+variable "mykey"{
+    default = "my_tf_keypair"
+}
+variable "vpc_id"{}
+variable "subnet_id"{}
+variable "vpc_security_group_ids"{
+  type = list(string)
+  default = []
+}
